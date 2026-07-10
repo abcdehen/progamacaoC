8aaebc224051c54ecf8c8ee9e0dc13af8c30e64c
abcdehen <henrirampazzo@gmail.com>
2026-07-10 19:55:58 +0000

    Fiz um código usando loops aninhados

---
+++ loop_aninhado.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int i, j;
+    char letra;
+    printf("*** Desenhando um triângulo com letras ***\n");
+    for (i = 1; i <= 5; i++){
+        letra = 'A';
+        for(j = 1; j <= i; j++){
+            printf("%c ", letra);
+            ++letra;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
